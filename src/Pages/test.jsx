654557340361812f6062651b1571c7935eb9628b
EBEREGIT <samsoneberenjoku@gmail.com>
2021-08-23 13:03:46 +0100

    back to top button added

---
+++ src/Pages/test.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import { useState } from "react";
+// import { button } from "@material-ui/core";1
+
+export default function BackToTop() {
+  const [state, setState] = useState(null);
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      setState(true);
+    } else {
+      setState(false);
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  return (
+    <>
+      <button
+        className="btn_green"
+        onClick={() => topFunction()}
+        id="back_to_top"
+        title="Go to top"
+        style={{ display: state ? "block" : "none" }}
+      >
+        <KeyboardArrowUpIcon style={{ fontSize: 40 }} />
+      </button>
+    </>
+  );
+}
