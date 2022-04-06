eff64c1dd13c84154101cfab3db9b68a9dba083b
eberegit <samsoneberenjoku@gmail.com>
2022-04-06 22:19:28 +0100

    updates

--- src/Components/HomeV2/Footer/Contact.jsx
+++ src/Components/HomeV2/Footer/Contact.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Content from "./Content";
-import { ImWhatsapp, ImEnvelop } from "react-icons/im";
-import { BiPhoneCall } from "react-icons/bi";
+import { ImEnvelop } from "react-icons/im";
+import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai";
 
 export default function Contact() {
   return (
     <>
       <Content
         heading={"Contact us"}
-        icon1={<ImWhatsapp />}
-        icon2={<BiPhoneCall />}
+        icon1={<AiFillLinkedin />}
+        icon2={<AiFillTwitterCircle />}
         icon3={<ImEnvelop />}
-        title1={"08011223344"}
-        title2={"08055667788"}
+        title1={"Linkedin"}
+        title2={"Twitter"}
         title3={"samsoneberenjoku@gmail.com"}
-        link1="https://wa.me/2348011223344"
-        link2="tel:2348055667788"
+        link1="https://www.linkedin.com/in/samson-ebere-njoku-profile/"
+        link2="https://twitter.com/eberetwit"
         link3="mailto:samsoneberenjoku@gmail.com"
       />
     </>
